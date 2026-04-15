ca8e0b3962a367035708fd2274f9afdb1ff7348d
Sebastian Martinez <sebimartinez3324@gmail.com>
2026-04-15 10:18:48 -0400

    Fun House Kattis Complete

--- APKattis/c4-implementation/FunHouse/main.cpp
+++ APKattis/c4-implementation/FunHouse/main.cpp
@@ -1,44 +1,62 @@
-#include <cstddef>
 #include <iostream>
-#include <set>
 
 using namespace std;
 
-void startDir(pair<int, int> ent, pair<int,int> &dir) {
-    if (ent.first == 0){
-        
+void chooseDirection(char m, pair<int, int> &dir) {
+    int dx = dir.first;
+    int dy = dir.second;
+    if (m == '/'){
+        dir = {dy, dx};
+    } else if (m == '\\') {
+        dir = {-dy,-dx};
     }
 }
 
 int main (){
+    
+    int num = 0;
+
     while(true) {
         int xmax, ymax;
         cin >> xmax >> ymax;
         if (xmax == 0 && ymax == 0) { break; }
 
+        num++;
+        cout << "HOUSE " << num << '\n';
+
         char map[ymax][xmax];
         string line;
 
-        pair<int, int> ent;
+        pair<int, int> pos;
         pair<int, int> dir;
 
-        for (int row = ymax; row > 0; row--) {
+        for (int row = ymax-1; row >= 0; row--) {
             cin >> line;
             for (int col = 0; col < xmax; col++) {
                 map[row][col] = line[col];
-                if (line[col] == '*') { ent = {col, row}; }
+                if (line[col] == '*') { pos = {col, row}; }
             }
         }
-        // cout << ent.first << " " << ent.second << '\n';
 
-    
-        for (int row = ymax; row > 0; row--){
+        if(pos.first == xmax-1) { dir = {-1, 0}; }
+        else if(pos.first == 0) { dir = {1, 0}; }
+        else if(pos.second == ymax-1) { dir = {0, -1}; }
+        else if(pos.second == 0) { dir = {0, 1}; }
+
+        while (map[pos.second][pos.first] != 'x'){
+            pos = {pos.first+dir.first, pos.second+dir.second};
+            char ch = map[pos.second][pos.first];
+            if (ch != '.') { chooseDirection(ch, dir); }
+        }
+
+        map[pos.second][pos.first] = '&';
+
+        for (int row = ymax-1; row >= 0; row--){
             for (int col = 0; col < xmax; col++) {
                 cout << map[row][col];
             }
             cout << '\n';
         }
-
     }
 }
 
