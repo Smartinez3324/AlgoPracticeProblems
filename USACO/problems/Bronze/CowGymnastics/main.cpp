60aa1fe20a75c03b12fb747fa3c5f63299f938b8
Sebastian Martinez <sebastian.martinez@honeywell.com>
2026-04-16 16:50:42 -0400

    working on Gymnastics

---
+++ USACO/problems/Bronze/CowGymnastics/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    ifstream fin("gymnastics.in");
+    ofstream fout("gymnastics.out");
+
+    int k, n;
+    fin >> k >> n;
+
+    int score[k][n];
+    set<pair<int, int>> pairs; 
+    
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < n; j++) { 
+            fin >> score[i][j];
+        }
+    }
+    
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            pairs.insert({score[0][i], score[0][j]});
+        }
+    }
+
+    for (int x = 1; x < k; x++) {
+        for (int i = 0; i < n-1; i++) {
+            for (int j = i+1; j < n; j++) {
+                pairs.erase({score[x][i], score[x][j]});
+            }
+        }
+    }
+
+
+    for (auto i : pairs) {
+        cout << i.first << ' ' << i.second << '\n';
+    } 
+
+}
